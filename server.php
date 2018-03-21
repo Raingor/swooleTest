dc85656203f5875a0fe86a6a3b1968ad3252eb3c
raingor <raingor@aliyun.com>
2018-03-21 11:09:16 +0800

    日常训练

--- server.php
+++ server.php
@@ -13,9 +13,8 @@ $server->on('open',function($server,$fd){
     $fromUser = $fd->fd;
     echo "成功握手======".$fromUser."\n";
     $server->push($fromUser,"成功握手");
-    foreach ($server->connections as $toUser) {
-        $server->push($toUser,"用户".$fromUser."已上线");
-    }
+    getCurrentUser();
+   noticeUp($fromUser);
 });
 
 /**
@@ -33,11 +32,38 @@ $server->on('close',function($server,$fd){
     $fromUser = $fd->fd;
     echo $fromUser."已断开连接\n";
     $server->push($fromUser,"已和服务器断开连接");
+    noticeDown($fromUser);
+});
+
+/**
+ * 通知上线
+ */
+function noticeUp($fromUser){
     foreach ($server->connections as $toUser) {
+        $server->push($toUser,"用户".$fromUser."已上线");
+    }
+}
+/**
+ * 通知下线
+ */
+function noticeDown($fromUser){
+foreach ($server->connections as $toUser) {
         assert($fromUser!=$toUser);
         $server->push($toUser,$fromUser."已下线");
     }
-});
+
+}
+
+
+/**
+ * 获取当前用户列表
+ */
+function getCurrentUser(){
+    foreach ($server->connections as $user) {
+        $userid.="$user,";
+    }
+    $server->push($user,"当前用户列表".$userid);
+}
 
 
 $server->start();
