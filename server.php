6f9362189ee018e49bcdb1df92b13be0bf830bb9
raingor <raingor@aliyun.com>
2018-03-21 16:35:18 +0800

    聊天服务修改

--- server.php
+++ server.php
@@ -10,17 +10,7 @@ $server = new swoole_websocket_server($host,$port);
 require_once('common.php');
 
 $server->on('open',function($server,$fd){
-    $fromUser = $fd->fd;
     echo "成功握手======".$fromUser."\n";
-    $server->push($fromUser,"成功握手");
-    $userid = '';
-    foreach ($server->connections as $user) {
-        $userid.="$user,";
-    }
-    $server->push($fromUser,"当前用户列表".$userid);
-    foreach ($server->connections as $toUser) {
-        $server->push($toUser,"用户".$fromUser."已上线");
-    }
 });
 
 /**
@@ -43,11 +33,6 @@ $server->on('message',function($server,$fd){
  */
 $server->on('close',function($server,$fd){
     echo $fd."已断开连接\n";
-    $server->push($fd,"已和服务器断开连接");
-    foreach ($server->connections as $toUser) {
-        assert($fd!=$toUser);
-        $server->push($toUser,$fd."已下线");
-    }
 });
 
 
